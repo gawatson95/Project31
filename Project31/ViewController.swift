ed08f717606b7e02c85a5a6a7df3450f52cf33aa
Grant Watson <gaw922@gmail.com>
2022-11-07 16:21:28 -0600

    Initial Commit

---
+++ Project31/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  Project31
+//
+//  Created by Grant Watson on 11/7/22.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate {
+
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var addressBar: UITextField!
+    
+    weak var activeWebView: WKWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setDefaultTitle()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWebView))
+        let delete = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteWebView))
+        navigationItem.rightBarButtonItems = [delete, add]
+    }
+
+    func setDefaultTitle() {
+        title = "Press + in the top right corner to open a webpage"
+    }
+    
+    @objc func addWebView() {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        stackView.addArrangedSubview(webView)
+        
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        
+        webView.layer.borderColor = UIColor.blue.cgColor
+        selectWebView(webView)
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(webViewTapped))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
+    }
+    
+    func selectWebView(_ webView: WKWebView) {
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+        
+        activeWebView = webView
+        webView.layer.borderWidth = 3
+        
+        updateTitle(for: webView)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let webView = activeWebView, var address = addressBar.text {
+            if !address.hasPrefix("https://") {
+                address = "https://" + address
+            }
+            
+            if let url = URL(string: address) {
+                webView.load(URLRequest(url: url))
+            }
+        }
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func deleteWebView() {
+        if let webView = activeWebView {
+            if let index = stackView.arrangedSubviews.firstIndex(of: webView) {
+                webView.removeFromSuperview()
+                
+                if stackView.arrangedSubviews.count == 0 {
+                    setDefaultTitle()
+                } else {
+                    var currentIndex = Int(index)
+                    
+                    if currentIndex == stackView.arrangedSubviews.count {
+                        currentIndex = stackView.arrangedSubviews.count - 1
+                    }
+                    
+                    if let newSelectedWebView = stackView.arrangedSubviews[currentIndex] as? WKWebView {
+                        selectWebView(newSelectedWebView)
+                    }
+                }
+            }
+        }
+    }
+    
+    @objc func webViewTapped(_ recognizer: UITapGestureRecognizer) {
+        if let selectedWebView = recognizer.view as? WKWebView {
+            selectWebView(selectedWebView)
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.horizontalSizeClass == .compact {
+            stackView.axis = .vertical
+        } else {
+            stackView.axis = .horizontal
+        }
+    }
+    
+    func updateTitle(for webView: WKWebView) {
+        title = webView.title
+        addressBar.text = webView.url?.absoluteString ?? ""
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if webView == activeWebView {
+            updateTitle(for: webView)
+        }
+    }
+}
+
